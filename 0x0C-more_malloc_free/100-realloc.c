7c9a0f1d368f7e26a8ea22d13b6b147d699a7e22
esubalew3883 <100286744+esubalew3883@users.noreply.github.com>
2022-04-07 20:22:58 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,49 +1,47 @@
-#include "main.h"
-#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * string_nconcat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
- * @n: amount of bytes.
- *
- * Return: pointer to the allocated memory.
- * if malloc fails, status value is equal to 98.
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: input pointer
+ * @old_size: size of old ptr
+ * @new_size: size of new ptr
+ * Return: reallocated ptr
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+	void *res = NULL;
 
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
-
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
-
-	if (n > ls2)
-		n = ls2;
-
-	lsout = ls1 + n;
-
-	sout = malloc(lsout + 1);
-
-	if (sout == NULL)
+	if (new_size == old_size)
+		return (ptr);
+	if (!ptr)
+	{
+		free(ptr);
+		res = malloc(new_size);
+		return (res);
+	}
+	if (!new_size && ptr)
+	{
+		free(ptr);
 		return (NULL);
+	}
+	res = malloc(new_size);
+	_memcpy(res, ptr, old_size);
+	free(ptr);
+	return (res);
+}
 
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
+/**
+ * _memcpy - copies memory area
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be copied
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ptr = dest;
 
-	sout[i] = '\0';
-
-	return (sout);
+	while (n--)
+		*dest++ = *src++;
+	return (ptr);
 }
